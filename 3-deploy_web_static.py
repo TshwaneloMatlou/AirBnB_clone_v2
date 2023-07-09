77dfaa9d3c0443b4be02c24f63ebe59cbcbab13f
TshwaneloMatlou <tshwanelomatlou55@gmail.com>
2023-07-09 08:08:04 -0700

    0x03. AirBnB clone - Deploy static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['54.89.27.4', '54.146.65.104']
 
 
 def do_pack():
